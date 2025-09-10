efee62465e16caaa7297433804823e54ca353d91
Theo Samuel Dicunawi Aritonang <theotahu99@gmail.com>
2025-09-10 17:42:26 +0700

    upd cookies

--- netlify/functions/instagram_tools.js
+++ netlify/functions/instagram_tools.js
@@ -17,7 +17,7 @@ export const handler = async (event, context) => {
     const headers = {
       'User-Agent': 'Mozilla/5.0',
       // PENTING: Pastikan Anda menggunakan cookie yang valid dan terbaru di sini!
-      'Cookie': 'ci_session=xxxxxxxx_NILAI_COOKIE_VALID_TERBARU_ANDA_xxxxxxxxx'
+      'Cookie': 'cw_conversation=eyJhbGciOiJIUzI1NiJ9.eyJzb3VyY2VfaWQiOiIwMmQyOGI0Zi00ODA3LTQ2NTMtOWY1ZS1jZDNlYmFiZTRjNWYiLCJpbmJveF9pZCI6MX0.wAXs3_d_STfrhpGQ-CH0Ug2fkQ5A1gC7-HYzVq1GNoI;ci_session=9o9job87hmg9skfat8cap41t788ildhk;csrf_cookie=2cfc4b4fe9aec692dfbf2c9dae471140'
     };
 
     const apiResponse = await axios.get(targetUrl, {
